3693a2dd997867cbcb62b6bb71c78f07020b4fd1
gusholguin <augustin.holguin@gmail.com>
2018-02-13 17:28:06 +0100

    separation of js in a new file

---
+++ js/app.js
@@ -0,0 +1,49 @@
+var app = new Vue ({
+  el: '#app',
+  data () {
+    return {
+      about: true,
+      experience: false,
+      portfolio: false,
+      music: false,
+      contact: false
+    }
+  },
+  methods: {
+    showAbout () {
+      this.about = true
+      this.experience = false
+      this.portfolio = false
+      this.contact = false
+      this.music = false
+    },
+    showExperience () {
+      this.experience = true
+      this.portfolio = false
+      this.contact = false
+      this.about = false
+      this.music = false
+    },
+    showPortfolio () {
+      this.portfolio = true
+      this.experience = false
+      this.contact = false
+      this.about = false
+      this.music = false
+    },
+    showMusic () {
+      this.music = true
+      this.portfolio = false
+      this.experience = false
+      this.contact = false
+      this.about = false
+    },
+    showContact () {
+      this.contact = true
+      this.experience = false
+      this.portfolio = false
+      this.about = false
+      this.music = false
+    }
+  }
+})
