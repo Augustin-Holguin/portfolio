04c25d168b51cc2357e6808e7565c3cb11851805
gusholguin <augustin.holguin@gmail.com>
2018-02-15 14:02:12 +0100

    navbar color update

--- particles/particle2.js
+++ particles/particle2.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": ["#d7d7d8", "#669B7C", "#F2D1A8"]
+        "value": ["#d7d7d8", "#537791", "#F2D1A8"]
       },
       "shape": {
         "type": "circle",
